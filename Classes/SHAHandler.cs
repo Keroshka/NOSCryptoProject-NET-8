f28162a48d62f54b6ec610697946c0797fa55122
Ivan Kerovec <ivankerovec2@gmail.com>
2023-11-30 12:20:21 +0100

    Added SHA window and started implementation of SHA class.

---
+++ Classes/SHAHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace NOSCryptoProject.Classes
+{
+    public class SHAHandler
+    {
+
+        private readonly SHA256 _sha;
+        public SHAHandler()
+        {
+            _sha = SHA256.Create();
+        }
+
+        public byte[] HashPlainText(string plainText)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(plainText);
+            return _sha.ComputeHash(bytes);
+        }
+    }
+}
---
+++ Windows/wdSHA.xaml.cs
@@ -0,0 +1,33 @@
+﻿using OSCryptoProject.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NOSCryptoProject.Windows
+{
+    /// <summary>
+    /// Interaction logic for wdSHA.xaml
+    /// </summary>
+    public partial class wdSHA : Window
+    {
+        private readonly RSAHandler _rsa;
+
+
+        public wdSHA()
+        {
+            InitializeComponent();
+            _rsa = new RSAHandler();
+        }
+    }
+}
